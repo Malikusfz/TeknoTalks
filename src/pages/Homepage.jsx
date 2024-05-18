7a85e1171376a46ba6dd1c3a633bc1a43394e30d
Malikus Syafaadi Nurfaza <malikusnurfaza@apps.ipb.ac.id>
2024-05-18 18:39:13 +0700

    test

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,3 +1,4 @@
+// Menghapus useState untuk menyebabkan error
 import {
   Box,
   Container,
@@ -9,9 +10,9 @@ import { LuMessageSquarePlus } from 'react-icons/lu';
 import { Link } from 'react-router-dom';
 import { useQuery, useMutation, useQueryClient } from 'react-query';
 import { useSelector } from 'react-redux';
-import { useState } from 'react';
-import ThreadsList from '../components/ThreadsList';
-import api from '../utils/api';
+// import { useState } from 'react'; // Hapus useState untuk menyebabkan error
+import ThreadsList dari '../components/ThreadsList';
+import api dari '../utils/api';
 
 const fetchThreadsAndUsers = async () => {
   const [threads, users] = await Promise.all([api.seeAllThreads(), api.getAllUsers()]);
@@ -35,7 +36,7 @@ function Homepage() {
   const { data, error, isLoading } = useQuery('threadsAndUsers', fetchThreadsAndUsers, {
     staleTime: 1000 * 60 * 5, // Cache the data for 5 minutes
   });
-  const [localVotes, setLocalVotes] = useState({});
+  // const [localVotes, setLocalVotes] = useState({}); // Hapus useState untuk menyebabkan error
 
   const upVoteMutation = useMutation(toggleUpVoteThread, {
     onSuccess: () => queryClient.invalidateQueries('threadsAndUsers'),
@@ -65,36 +66,37 @@ function Homepage() {
     ? threads.map((thread) => ({
       ...thread,
       user: users.find((user) => user.id === thread.ownerId),
-      votes: localVotes[thread.id] !== undefined ? localVotes[thread.id] : thread.votes,
+      // votes: localVotes[thread.id] !== undefined ? localVotes[thread.id] : thread.votes, // Hapus untuk menyebabkan error
+      votes: thread.votes, // Sementara set ke thread.votes untuk kompilasi, ini juga akan menyebabkan error pada localVotes
     }))
     : [];
 
   const voteHandler = (threadId, action) => {
-    const previousVotes = localVotes[threadId] !== undefined ? localVotes[threadId] : threads.find((thread) => thread.id === threadId).votes;
+    const previousVotes = threads.find((thread) => thread.id === threadId).votes;
     let newVotes;
 
     if (action === 'upvote') {
       newVotes = previousVotes + 1;
-      setLocalVotes((prev) => ({ ...prev, [threadId]: newVotes }));
+      // setLocalVotes((prev) => ({ ...prev, [threadId]: newVotes })); // Hapus untuk menyebabkan error
       upVoteMutation.mutate(threadId, {
         onError: () => {
-          setLocalVotes((prev) => ({ ...prev, [threadId]: previousVotes }));
+          // setLocalVotes((prev) => ({ ...prev, [threadId]: previousVotes })); // Hapus untuk menyebabkan error
         },
       });
     } else if (action === 'downvote') {
       newVotes = previousVotes - 1;
-      setLocalVotes((prev) => ({ ...prev, [threadId]: newVotes }));
+      // setLocalVotes((prev) => ({ ...prev, [threadId]: newVotes })); // Hapus untuk menyebabkan error
       downVoteMutation.mutate(threadId, {
         onError: () => {
-          setLocalVotes((prev) => ({ ...prev, [threadId]: previousVotes }));
+          // setLocalVotes((prev) => ({ ...prev, [threadId]: previousVotes })); // Hapus untuk menyebabkan error
         },
       });
     } else if (action === 'neutralize') {
       newVotes = previousVotes;
-      setLocalVotes((prev) => ({ ...prev, [threadId]: newVotes }));
+      // setLocalVotes((prev) => ({ ...prev, [threadId]: newVotes })); // Hapus untuk menyebabkan error
       neutralizeVoteMutation.mutate(threadId, {
         onError: () => {
-          setLocalVotes((prev) => ({ ...prev, [threadId]: previousVotes }));
+          // setLocalVotes((prev) => ({ ...prev, [threadId]: previousVotes })); // Hapus untuk menyebabkan error
         },
       });
     }
@@ -131,7 +133,6 @@ function Homepage() {
       </Container>
     </Box>
   );
-  return <div>Text yang salah untuk pengujian</div>;
 }
 
 export default Homepage;
